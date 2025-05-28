b6187597bfd2359ca3db7fd882a0dbc29b0b99f2
imurar <imurar@outlook.com>
2025-05-29 07:15:49 +0900

    今回のアプリに必要なテーブルをすべてマイグレーション

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ Route::get('/', function () {
 }) -> name('top');
 
 Route::get('/teams', [MTeamController::class, 'index'])->name('teams.index');
+Route::get('/teams/{id}', [MTeamController::class, 'show'])->name('teams.show');
+Route::get('/teams/{id}/players', [MTeamController::class, 'players'])->name('teams.players');
 
 Route::get('/players', [MPlayerController::class, 'index'])->name('players.index');
 Route::get('/players/create', [MPlayerController::class, 'create'])->name('players.create');
 Route::post('/players',[MPlayerController::class, 'store'])->name('players.store');
+
+
