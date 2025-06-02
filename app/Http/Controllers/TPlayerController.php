bea5b39c4bf33221317cb4660680f38bc9463127
imurar <imurar@outlook.com>
2025-06-02 11:40:24 +0900

    players.storeにて登録ロジックの実装

--- app/Http/Controllers/TPlayerController.php
+++ app/Http/Controllers/TPlayerController.php
@@ -26,8 +26,26 @@ class TPlayerController extends Controller
         return view('players.create', compact('team', 'positions', 'prefectures', 'citys'));
     }
 
-    public function store(Request $request){
+    public function store(Request $request, $team_id){
+        //バリテーション
+        $validated  = $request->validate([
+            'name' => 'required|string|max:255',
+            'position_id' => 'required|string|max:255',
+            'uniform_no' => 'required|string|max:10',
+            'highschool' => 'nullable|string|max:255',
+            'university' => 'nullable|string|max:255',
+            'birthday' => 'nullable|string|max:255',
+            'prefecture_id' => 'nullable|string|max:255',
+            'city_id' => 'nullable|string|max:255',
+        ]);
 
+        //team_idは固定なので後から追加
+        $validated['team_id']=$team_id;
+
+        TPlayer::create($validated);
+
+        return redirect()->route('players.index', ['team_id' => $team_id])
+                        ->with('success', '選手が登録されました。');
     }
 
     public function show(){
