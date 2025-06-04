5b44d40100286546f891a8dd5d597f5ea53ee800
imurar <imurar@outlook.com>
2025-06-04 10:17:18 +0900

    vueを使って球団一覧ページの表示

--- app/Http/Controllers/MTeamController.php
+++ app/Http/Controllers/MTeamController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\MTeam;
+use Inertia\Inertia; //vue導入で必要
 
 class MTeamController extends Controller
 {
     //
     public function index() {
         $teams = MTeam::all();
-        return view('teams.index' , compact('teams'));
+        //return view('teams.index' , compact('teams'));
+        return Inertia::render('Teams/Index',['teams' => $teams,]);
     }
 
     public function show($team_id){
         $team = MTeam::findOrFail($team_id);
-        return view('teams.show', compact('team'));
+        //return view('teams.show', compact('team'));
+        return Inertia::render('Teams/Show',['team' => $team,]);
     }
 
     //public function players($team_id){
---
+++ resources/js/Pages/Teams/Index.vue
@@ -0,0 +1,36 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps ({ teams:Array, });
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <template #header>
+            <h1>球団一覧</h1>
+        </template>
+            
+        <div>
+            <table border="1" cellpadding="5">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>球団名</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="team in teams" :key="team.id">
+                        <td>{{ team.id }}</td>
+                        <td>
+                            <Link :href="route('teams.show', {team_id: team.id})">
+                                {{ team.name }}
+                            </Link>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Link :href="route('dashboard')">戻る</Link><br /> 
+        </div> 
+    </AuthenticatedLayout>
+</template>
