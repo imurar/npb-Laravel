e9161e00d07765dde2f6eef6625a5629fcb276b3
imurar <imurar@outlook.com>
2025-07-06 13:32:06 +0900

    プロジェクトをリセット

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\MCityController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('prefectures/{prefecture}/citys', [MCityController::class, 'getByPrefecture']);
