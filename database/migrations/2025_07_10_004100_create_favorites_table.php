767b8ee9d07e12711d74b505869ed693d2bc8761
imurar <imurar@outlook.com>
2025-07-10 13:46:37 +0900

    feat: create favorites table

--- database/migrations/2025_07_10_004100_create_favorites_table.php
+++ database/migrations/2025_07_10_004100_create_favorites_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('player_id')->constrained()->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
