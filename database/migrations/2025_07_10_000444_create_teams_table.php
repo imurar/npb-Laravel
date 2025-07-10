85997754410d46ecb50173cd6b18d096b3af4818
imurar <imurar@outlook.com>
2025-07-10 13:43:47 +0900

    feat: create teams table

--- database/migrations/2025_07_10_000444_create_teams_table.php
+++ database/migrations/2025_07_10_000444_create_teams_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('abbreviation')->nullable();
+            $table->string('logo_url')->nullable();
             $table->timestamps();
         });
     }
