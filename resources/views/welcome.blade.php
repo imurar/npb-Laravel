c909fed2a97185906ec5129215833d9da5d74220
imurar <imurar@outlook.com>
2025-07-13 14:33:07 +0900

    feat: Vueコンポーネント描画用のdivとViteのJS読み込みを追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,9 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        <!-- vueをマウントする場所 -->
+        <div id="example"></div>
+        
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
@@ -273,5 +276,8 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
+
+    <!-- viteでビルドされたJS読み込み -->
+    @vite('resources/js/app.js')
     </body>
 </html>
