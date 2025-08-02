7e58b19ef5563186b41389b4973a464880feb59b
imurar <imurar@outlook.com>
2025-08-02 14:53:26 +0900

    feat: Positionモデルの作成

--- app/Models/MPosition.php
+++ app/Models/MPosition.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class MPosition extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    //TPlayerとのリレーション
+    //Position 1 に属するプレイヤーが複数いる場合、このリレーションで
+    //position->players で全プレイヤーが取得できる。
+    public function players() {
+        return $this->hasMany(TPlayer::class, 'position_id');
+    }
 }
