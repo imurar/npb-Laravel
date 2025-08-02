b3f694db442cf4087539bbb48ea26a755fe45168
imurar <imurar@outlook.com>
2025-08-02 14:54:07 +0900

    feat: Teamモデルの作成

--- app/Models/MTeam.php
+++ app/Models/MTeam.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class MTeam extends Model
 {
-    //
+    protected $fillable = ['name'];
+    
+    public function players(){
+        return $this->hasMany(TPlayer::class, 'team_id');
+    }
 }
