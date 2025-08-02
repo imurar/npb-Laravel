5294ad494b01b8ed031e6f08dec7b7e98dc5ec09
imurar <imurar@outlook.com>
2025-08-02 14:53:15 +0900

    feat: Cityモデルの作成

--- app/Models/MCity.php
+++ app/Models/MCity.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class MCity extends Model
 {
-    //
+    protected $fillable = ['name', 'prefectures_id'];
+
+    public function players() {
+        return $this->hasMany(TPlayer::class, 'city_id');
+    }
 }
