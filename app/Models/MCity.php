d38d17a7be8a5feff513b3082f43a3f2a05fe4f3
imurar <imurar@outlook.com>
2025-08-03 16:27:11 +0900

    fix: 項目名のタイポ修正

--- app/Models/MCity.php
+++ app/Models/MCity.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class MCity extends Model
 {
-    protected $fillable = ['name', 'prefectures_id'];
+    protected $table = 'm_cities';
+    protected $fillable = ['name', 'prefecture_id'];
 
     public function players() {
         return $this->hasMany(TPlayer::class, 'city_id');
