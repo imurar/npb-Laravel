e830ef22b234b2d2c4c2dbd3ee28a10ae82cfee6
imurar <imurar@outlook.com>
2025-08-03 11:10:11 +0900

    refactor: MCityControllerに都道府県ごとの市区町村取得処理を追加

--- app/Http/Controllers/MCityController.php
+++ app/Http/Controllers/MCityController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class MCityController extends Controller
 {
-    //
+    public function getByPrefecture($prefecture)
+    {
+        $cities = MCity::where('prefectures_id', $prefecture)->get();
+        return response()->json($cities);
+    }
 }
