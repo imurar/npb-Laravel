11e9332da5818071a4cd36283dfde60ef35a0296
imurar <imurar@outlook.com>
2025-08-03 16:27:36 +0900

    fix: prefecture_id 項目名のタイポ修正

--- app/Http/Controllers/MCityController.php
+++ app/Http/Controllers/MCityController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MCity;
 
 class MCityController extends Controller
 {
     public function getByPrefecture($prefecture)
     {
-        $cities = MCity::where('prefectures_id', $prefecture)->get();
+        $cities = MCity::where('prefecture_id', $prefecture)->get();
         return response()->json($cities);
     }
 }
